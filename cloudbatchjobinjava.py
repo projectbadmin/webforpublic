1359c59f92c3eff2eac3b314423b7390f54622d4
Patrick F <projectb.development.1@gmail.com>
2024-10-29 08:59:57 +0800

    add the logic to read the javap file

--- cloudbatchjobinjava.py
+++ cloudbatchjobinjava.py
@@ -11,6 +11,7 @@ classMethods = {
     "abc": ["method1", "method2", "method3"]
 };
 
+
 def check_and_generate_keywords_(line, cursor_pos):
     match = re.search(r'(\w+)\.$', line[:cursor_pos])
     if match:
@@ -23,3 +24,38 @@ def check_and_generate_keywords_(line, cursor_pos):
         filtered_list = [kw for kw in commonJavaKeyWords if kw.startswith(line[cursor_pos:])]
     
     return filtered_list
+
+
+def read_javap_result(file_path):
+    try:
+        tempClassName = ''
+        tempClass = []
+        with open(file_path, 'r') as file:
+            for line in file:
+                content = line.strip()
+                # detect the class
+                if ' class ' in content and ' {' in content:
+                    tempClassName = content.replace('public class ', '').replace(' {', '').strip()
+                    result[tempClassName] = {}
+                    tempClass = result[tempClassName]
+                    tempClass['variables'] = set()
+                    tempClass['methods'] = set()
+                if len(content) > 0 and 'public' in content and tempClassName not in content:
+                    if '(' not in content:
+                        tempClass['variables'].add(content.replace('public ', '').replace(';', ''))
+                    else:  
+                        tempMethod = content.replace('public ', '').replace(';', '')
+                        tempMethod = tempMethod[:tempMethod.index(')') + 1]
+                        tempClass['methods'].add(tempMethod)
+
+        result.pop('main.logiclibrary.ForFutureData')
+        result.pop('tradelibrary.Order')
+        result.pop('tradelibrary.Profile')
+        result.pop('Main')
+        result['public final class orderlibirary.Action extends java.lang.Enum<orderlibirary.Action>'].pop('methods')
+
+        print(result)
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
