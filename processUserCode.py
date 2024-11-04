aa9e7d5e6f1a4785da06996253a84ec36cc63be6
patrick9577 <57570834+patrick9577@users.noreply.github.com>
2024-11-04 22:30:32 +0800

    copy logic file and jar file only

--- processUserCode.py
+++ processUserCode.py
@@ -57,9 +57,9 @@ def process(app, code_for_onStart, code_for_onProcess, code_for_onEnd, requestid
             app.logger.info(f"Ran {destination_dir}{requestid}-0.0.1-SNAPSHOT-jar-with-dependencies.jar")
         if app.config['env'] in ['ec2instance', 'beanstalkinstance']:
             # create S3 folder for the requestid            
-            subprocess.run([f"aws s3 cp {app.config['clone_of_cloudBatchJobTemplate']}{requestid}/ s3://projectbcloudbatchjobprogramfile/{requestid}/ --recursive"], capture_output=True, text=True, shell=True, env=env)            # sync the folder to S3
+            subprocess.run([f"aws s3 cp {app.config['clone_of_cloudBatchJobTemplate']}{requestid}/cloudBatchJobInJava/src/main/java/main/ s3://projectbcloudbatchjobprogramfile/{requestid}/ --recursive"], capture_output=True, text=True, shell=True, env=env)            # sync the folder to S3
+            subprocess.run([f"aws s3 cp {app.config['clone_of_cloudBatchJobTemplate']}{requestid}/cloudBatchJobInJava/target/cloudBatchJobInJava-0.0.1-SNAPSHOT-jar-with-dependencies.jar s3://projectbcloudbatchjobprogramfile/{requestid}/"], capture_output=True, text=True, shell=True, env=env)            # sync the folder to S3
 
-    
     except Exception as e:
         app.logger.error(e)
     finally:
