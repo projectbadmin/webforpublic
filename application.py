d7d0b1b5e4140f50021e2808b70e5811d92d5814
Patrick F <projectb.development.1@gmail.com>
2024-11-22 14:30:53 +0800

    bugfix

--- application.py
+++ application.py
@@ -162,7 +162,7 @@ def filter_streams():
     retention_hour = data['retention-hour']
     class_code = data['class-code']
     filtered_list = get_dataStreamingList(stream_status, retention_hour, class_code, "")
-    return jsonify(filtered_list)
+    return render_template('home.html', data_streaming_list=filtered_list)
 
 # Register the function to run before each request
 @application.before_request
