bb89b7f8e290507f499d409ebe2f5c61341de7f1
Patrick F <projectb.development.1@gmail.com>
2024-11-22 16:02:14 +0800

    try stream_template 4

--- application.py
+++ application.py
@@ -162,7 +162,7 @@ def filter_streams():
     retention_hour = data['retention-hour']
     class_code = data['class-code']
     filtered_list = get_dataStreamingList(stream_status, retention_hour, class_code, "")
-    return Response(stream_template('home.html', data_streaming_list=filtered_list))
+    return stream_template('home.html', data_streaming_list=filtered_list)
 
 # Register the function to run before each request
 @application.before_request
