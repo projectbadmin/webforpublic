72f4d2295acc1b88b216aca0f46706ae7cbee807
patrick9577 <57570834+patrick9577@users.noreply.github.com>
2024-12-08 16:54:33 +0800

    bugfix

--- processUserCode.py
+++ processUserCode.py
@@ -63,7 +63,7 @@ def process(app, code_for_onStart, code_for_onProcess, code_for_onEnd, requestid
         # Use existing job definition and submit a new job with a new command
         job_definition_name = 'projectbcloudbatchjobprogramfile-job-defintion'
         job_queue_name = 'projectbcloudbatchjobprogramfile-fargate-job-queue'
-        job_name = uuid.uuid4()
+        job_name = str(uuid.uuid4())
 
         # Submit the job with a new command
         command_for_BatchJob = ""
