39c3518231c1873986209d3496b5c3444967f4a0
Patrick F <projectb.development.1@gmail.com>
2024-12-19 17:13:14 +0800

    use bootstrap

--- application.py
+++ application.py
@@ -39,7 +39,7 @@ def login():
             return redirect(url_for('home'))
         else:
             return "Invalid credentials"
-    return render_template('login.html', title='Sign In')
+    return render_template('sigin.html')
 
 @application.route('/logout', methods=['GET'])
 def logout():
